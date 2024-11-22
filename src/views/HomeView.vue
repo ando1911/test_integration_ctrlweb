a5c94bcdc11b00bd91e81904240f5ecd76e8092c
Andoniaina <harinirinamamyandoniaina@gmail.com>
2024-11-22 16:55:21 +0300

    home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="mr-1">
+    <img src="/image/thumbnail.jpg">
+  </div>
 </template>
