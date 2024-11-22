c7078462a958dd9c7eeb5eb907b170bace1f07f2
Andoniaina <harinirinamamyandoniaina@gmail.com>
2024-11-22 16:54:58 +0300

    header of components

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex flex-row">
+        <div class="mr-1">
+            <img src="/image/logo_fl.png">
+        </div>
+        <div>
+            <h5>FIGMALAND </h5>
+            <p> Font Links : </p>
+            <a href="">
+                https://en.bestfonts.pro/font/graphik
+            </a>
+            <p> Discover More Inspiring Design on 👇 </p>
+
+            <a href="">
+                http://figmaland.com
+            </a>
+            <p>If you have a request for more ui elements for this landing page ? send us an email on 👇</p>
+            <a href="mailto: hello@figmaland.com"> hello@figmaland.com</a>
+            <p>We will add it on future updates free of charges.</p>
+        </div>
+    </div>
+</template>
